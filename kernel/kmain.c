6916249b540f1487e92b7d9733e431ddd1261878
Keyvan M. Kambakhsh <keyvankambakhsh@gmail.com>
2017-07-14 17:53:23 +0430

    better greeting message

--- kernel/kmain.c
+++ kernel/kmain.c
@@ -2,11 +2,5 @@
 
 void kmain() {
 	clear_screen();
-	kprint("In yek matn ast!!!!fwebjbwefwehuwefbkjwefbjk");
-    kprint_at("Xxx", 1, 6);
-    kprint_at("Xxx", 1, 10);
-    kprint_at("This text spans multiple lines", 75, 10);
-    kprint_at("There is a line\nbreak", 0, 20);
-    kprint("There is a line\nbreak");
-    kprint_at("What happens when we run out of space?", 45, 24);
+	kprint("We are in the kernel now!\nWelcome to ZincOS!\n > ");
 }
